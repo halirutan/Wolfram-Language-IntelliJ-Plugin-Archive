4107641389bddd0c85f65ba905f3266b8b415654
Patrick Scheibe <pscheibe@trm.uni-leipzig.de>
2013-10-22 18:14:19 +0200

    ..

--- src/de/halirutan/mathematica/codeInsight/smartEnter/MathematicaSmartEnter.java
+++ src/de/halirutan/mathematica/codeInsight/smartEnter/MathematicaSmartEnter.java
@@ -1,25 +1,23 @@
 package de.halirutan.mathematica.codeInsight.smartEnter;
 
+import com.intellij.codeInsight.editorActions.smartEnter.SmartEnterProcessor;
 import com.intellij.lang.SmartEnterProcessorWithFixers;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 import org.jetbrains.annotations.NotNull;
 
 /**
  * @author patrick (10/21/13)
  */
-public class MathematicaSmartEnter extends SmartEnterProcessorWithFixers {
-
-  public MathematicaSmartEnter() {
-    addFixers(
-        new FunctionCallFixer(),
-        new ErrorFixer()
-    );
-  }
+public class MathematicaSmartEnter extends SmartEnterProcessor {
 
   @Override
   public boolean process(@NotNull Project project, @NotNull Editor editor, @NotNull PsiFile psiFile) {
-    return super.process(project, editor, psiFile);
+    PsiElement statementAtCaret = getStatementAtCaret(editor, psiFile);
+    return false;
+
   }
+
 }
