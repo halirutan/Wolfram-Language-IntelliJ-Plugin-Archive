b59930165931a9a07e7c1693171aa67fd4948ce9
halirutan <pscheibe@trm.uni-leipzig.de>
2015-09-01 05:17:05 +0200

    Feature of make argument lists of functions and Lists selectable with ExpanSelection

---
+++ src/de/halirutan/mathematica/codeinsight/editoractions/wordselection/ListSelectioner.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2015 Patrick Scheibe
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package de.halirutan.mathematica.codeinsight.editoractions.wordselection;
+
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiElement;
+import de.halirutan.mathematica.parsing.psi.api.lists.List;
+
+/**
+ * @author patrick (01.09.15)
+ */
+public class ListSelectioner extends AbstractSelectionFixer<List> {
+
+  @Override
+  public boolean canSelect(final PsiElement psiElement) {
+    return (psiElement.getParent().getParent() instanceof List) || (psiElement.getParent() instanceof List);
+  }
+
+  @Override
+  public boolean isType(final PsiElement psiElement) {
+    return psiElement instanceof List;
+  }
+
+  @Override
+  public TextRange getTextRange(final PsiElement element) {
+    if (element instanceof List) {
+      List list = (List) element;
+      return new TextRange(
+          list.getTextOffset() + 1,
+          list.getTextOffset() + list.getTextLength() - 1
+      );
+    }
+    return null;
+  }
+
+
+
+}
