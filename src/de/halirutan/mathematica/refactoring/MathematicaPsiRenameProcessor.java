351512d3e0ecccabf8bc18e31fe2dedab09c9ba0
Patrick Scheibe <patrick@halirutan.de>
2016-12-27 06:35:28 +0100

    Implemented a recursive visitor that checks each occurrence of a symbol for renaming

--- src/de/halirutan/mathematica/refactoring/MathematicaPsiRenameProcessor.java
+++ src/de/halirutan/mathematica/refactoring/MathematicaPsiRenameProcessor.java
@@ -23,19 +23,25 @@ package de.halirutan.mathematica.refactoring;
 
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiReference;
+import com.intellij.psi.util.PsiTreeUtil;
 import com.intellij.refactoring.rename.RenamePsiElementProcessor;
+import de.halirutan.mathematica.parsing.psi.MathematicaRecursiveVisitor;
 import de.halirutan.mathematica.parsing.psi.api.Symbol;
 import de.halirutan.mathematica.parsing.psi.api.string.MString;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Objects;
 
-/** This class shouldn't be necessary since usually Idea does a great job of renaming if you have
+/**
+ * This class shouldn't be necessary since usually Idea does a great job of renaming if you have
  * your symbol references working correctly. Unfortunately there is a bug that was introduces by some
  * optimisation {@see https://youtrack.jetbrains.com/issue/IDEA-165760}.
- *
+ * <p>
  * Therefore, this class just adds some references that were missed by Idea. This will be removed as soon
  * as the bug is fixed.
+ *
  * @author patrick (24.12.16).
  */
 public class MathematicaPsiRenameProcessor extends RenamePsiElementProcessor {
@@ -48,13 +54,40 @@ public class MathematicaPsiRenameProcessor extends RenamePsiElementProcessor {
   @Override
   public Collection<PsiReference> findReferences(PsiElement element) {
     final Collection<PsiReference> references = super.findReferences(element);
-    final PsiReference[] myReferences = element.getReferences();
-    for (PsiReference ref : myReferences) {
-      if (!references.contains(ref)) {
-        references.add(ref);
+    PsiReference elementRef = element.getReference();
+    PsiElement definitionElement;
+    if (elementRef != null) {
+      definitionElement = elementRef.resolve();
+      if (definitionElement instanceof Symbol) {
+        SymbolCollector collector = new SymbolCollector((Symbol) definitionElement);
+        element.getContainingFile().accept(collector);
+        references.addAll(collector.myReferences);
       }
     }
     return references;
+  }
+
+  private class SymbolCollector extends MathematicaRecursiveVisitor {
+    Collection<PsiReference> myReferences;
+    Symbol myDefinitionElement;
+
+    SymbolCollector(Symbol definitionElement) {
+      this.myDefinitionElement = definitionElement;
+      myReferences = new HashSet<>();
+    }
+
+    @Override
+    public void visitSymbol(Symbol symbol) {
+      if (symbol.getSymbolName().equals(myDefinitionElement.getSymbolName())) {
+        PsiReference reference = symbol.getReference();
+        if (reference != null) {
+          PsiElement resolve = reference.resolve();
+          if (resolve != null && resolve.equals(myDefinitionElement)) {
+            myReferences.add(reference);
+          }
+        }
+      }
+    }
   }
 
 }
