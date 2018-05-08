67ae62882cd4e35c12369105b2cea564b91469a8
Patrick Scheibe <patrick@halirutan.de>
2018-05-08 06:59:15 +0200

    Fix Completion importance for package functions
    Now, local variables and package functions that you defined yourself will
    always appear on the top of the list if they match what you have typed.

--- src/de/halirutan/mathematica/codeinsight/completion/MathematicaCompletionContributor.java
+++ src/de/halirutan/mathematica/codeinsight/completion/MathematicaCompletionContributor.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (c) 2013 Patrick Scheibe
+ * Copyright (c) 2018 Patrick Scheibe
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -7,16 +8,16 @@
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package de.halirutan.mathematica.codeinsight.completion;
@@ -34,9 +35,9 @@ import org.jetbrains.annotations.NotNull;
  */
 public class MathematicaCompletionContributor extends CompletionContributor {
 
-  public static final double LOCAL_VARIABLE_PRIORITY = 40;
-  public static final double GLOBAL_VARIABLE_PRIORITY = 35;
-  public static final double IMPORT_VARIABLE_PRIORITY = 30;
+  public static final double LOCAL_VARIABLE_PRIORITY = 10000;
+  public static final double GLOBAL_VARIABLE_PRIORITY = 9000;
+  public static final double IMPORT_VARIABLE_PRIORITY = 8000;
 
 
   public MathematicaCompletionContributor() {
